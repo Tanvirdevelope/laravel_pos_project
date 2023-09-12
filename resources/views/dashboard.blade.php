c7b1386c2e3f693a24616a66c967e44b0ba7bc59
Tanvirdevelop <tanvirchowdhury698@gmail.com>
2023-09-12 15:20:56 +0600

    uptodate

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,140 @@
+@extends('layout.main')
+
+@section('main_content')
+    
+<div class="row">
+
+			<!-- Earnings (Monthly) Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+					<div class="card border-left-primary shadow h-100 py-2">
+							<div class="card-body">
+									<div class="row no-gutters align-items-center">
+											<div class="col mr-2">
+													<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+															Total Users</div>
+													<div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+											</div>
+											<div class="col-auto">
+													<i class="fas fa-calendar fa-2x text-gray-300"></i>
+											</div>
+									</div>
+							</div>
+					</div>
+			</div>
+
+			<!-- Earnings (Monthly) Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+					<div class="card border-left-success shadow h-100 py-2">
+							<div class="card-body">
+									<div class="row no-gutters align-items-center">
+											<div class="col mr-2">
+													<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+															Total Products</div>
+													<div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+											</div>
+											<div class="col-auto">
+													<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+											</div>
+									</div>
+							</div>
+					</div>
+			</div>
+
+			<!-- Earnings (Monthly) Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+				<div class="card border-left-success shadow h-100 py-2">
+						<div class="card-body">
+								<div class="row no-gutters align-items-center">
+										<div class="col mr-2">
+												<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+														Total Sales</div>
+												<div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+										</div>
+										<div class="col-auto">
+												<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+										</div>
+								</div>
+						</div>
+				</div>
+		</div>
+
+
+			<!-- Pending Requests Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+					<div class="card border-left-warning shadow h-100 py-2">
+							<div class="card-body">
+									<div class="row no-gutters align-items-center">
+											<div class="col mr-2">
+													<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+														Total Purchases</div>
+													<div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+											</div>
+											<div class="col-auto">
+													<i class="fas fa-comments fa-2x text-gray-300"></i>
+											</div>
+									</div>
+							</div>
+					</div>
+			</div>
+
+
+			<!-- Pending Requests Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+				<div class="card border-left-primary shadow h-100 py-2">
+						<div class="card-body">
+								<div class="row no-gutters align-items-center">
+										<div class="col mr-2">
+												<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+														Total Receipts</div>
+												<div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+										</div>
+										<div class="col-auto">
+												<i class="fas fa-calendar fa-2x text-gray-300"></i>
+										</div>
+								</div>
+						</div>
+				</div>
+		</div>
+
+
+			<!-- Pending Requests Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+				<div class="card border-left-success shadow h-100 py-2">
+						<div class="card-body">
+								<div class="row no-gutters align-items-center">
+										<div class="col mr-2">
+												<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+														Total Payments</div>
+												<div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+										</div>
+										<div class="col-auto">
+												<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+										</div>
+								</div>
+						</div>
+				</div>
+		</div>
+
+
+			<!-- Pending Requests Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+					<div class="card border-left-warning shadow h-100 py-2">
+							<div class="card-body">
+									<div class="row no-gutters align-items-center">
+											<div class="col mr-2">
+													<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+														Cash In Hand</div>
+													<div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+											</div>
+											<div class="col-auto">
+													<i class="fas fa-comments fa-2x text-gray-300"></i>
+											</div>
+									</div>
+							</div>
+					</div>
+			</div>
+
+
+	</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 
     Route::group(['middleware' =>'auth'], function() {
 
-    Route::get('dashboard', function () {
-        return view('welcome');
-    });
+    Route::get('dashboard', '\App\Http\Controllers\DashboardController@index');
 
     Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
     
